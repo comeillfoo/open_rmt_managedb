3a033614447d20d6b91f0b7d8054d4a41110d06e
Leargy <sushkevich.2001@mail.ru>
2020-05-05 12:15:14 +0300

    Client now can normally react on closed server.Has been made some work on dispatching, receiver service.Added а standardized sharing packet

--- server/src/perusal/BookWorm.java
+++ server/src/perusal/BookWorm.java
@@ -1,38 +1,42 @@
 package perusal;
 
+import communication.ClientPackage;
 import communication.Segment;
 import communication.Mediator;
 
 import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.IllegalBlockingModeException;
 import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 
 public final class BookWorm extends QueryReader {
   // fields
+  private ByteArrayInputStream byteArrayInputStream;
+  private ObjectInputStream objectInputStream;
+  private ByteBuffer byteBuffer = ByteBuffer.allocate(2*1024);
   // builders
   public BookWorm(Mediator m) { super(m); }
   // methods
   @Override
   public void retrieve(Segment parcel) {
-    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+    byteBuffer.clear();
     SocketChannel tempChannel = parcel.getClient();
     try {
-    if(tempChannel.read(byteBuffer) == -1) {
-      tempChannel.finishConnect();
-      tempChannel.socket().close();
-      return;
-    }
-    // TODO: бросает IllegalBlockingModeException после получения сообщения от клиента и после отключения клиента(второй случай выходит если Receptionist возвращает null)
-      SocketChannel instream = parcel.getClient();
-      //Command query = (Command)
-      instream.read(byteBuffer);
-      Arrays.asList(byteBuffer).forEach(a -> System.out.println(a));
-      //mediator.notify(this, new Segment(parcel.getClient(), query));
-    } catch (IOException e) {
+      tempChannel.read(byteBuffer);
+      byteBuffer.flip();
+      byteArrayInputStream = new ByteArrayInputStream(byteBuffer.array());
+      objectInputStream = new ObjectInputStream(byteArrayInputStream);
+      ClientPackage query = (ClientPackage) objectInputStream.readObject();
+      mediator.notify(this, new Segment(parcel.getClient(), (Serializable) query));
+    } catch ( IOException e) {
+      mediator.notify(this,parcel); //This class got nothing, we lost.
       // TODO: логировать ошибку
       // TODO: отправить отчет посреднику, чтобы тот уведомил клиента (возможно, только для режима debug)
-    //} catch (ClassNotFoundException e) {
+    } catch (ClassNotFoundException e) {
       // TODO: логировать ошибку
       // TODO: отправить отчет посреднику, чтобы тот уведомил клиента (возможно, только для режима debug)
     }
